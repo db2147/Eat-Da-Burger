40f7f9606cdabbdf5ccb2c4e738e693dc52358eb
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-21 18:19:22 -0600

    creating server.js page

--- server.js
+++ server.js
@@ -1,3 +1,50 @@
 var express = require("express");
 var methodoverride = require("method-override");
 var bodyparser = require("body-parser");
+
+// create our server
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(methodOverride('_method'));
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+// create connection to port
+var port = 3000;
+app.listen(port);
+
+// create mysql connection
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'burgers_db'
+});
+
+connection.connect(function(err){
+    if(err)throw err;
+    console.log('Connected as id: '+connection.threadId);
+})
+
+app.get('/',function(req,res){
+    connection.query('SELECT * FROM burgers;',function(err,data){
+        res.render('index',{movies:data});
+    })
+})
+
+app.post('/create', function(req,res){
+    connection.query('INSERT INTO burgers (burger) VALUES (?);s', [req.body.movie], function(err,result){
+        if(err)throw err;
+        res.redirect('/');   // redirects to the main page when the query goes off
+    })
+})
+
+app.put('/update',function(req,res){
+    connection.query('UPDATE burgers SET burger = ? WHERE id = ?;', [req.body.
+    movie, req.body.id], function(err,results){
+        if(err)throw err;
+        res.redirect('/');
+    })
+})
