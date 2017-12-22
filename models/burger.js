27ae86c715150450d2129f25d325cb8cff0e9a50
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-22 13:56:10 -0600

    create burger.js file

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,24 @@
-var connection = require("./burger.js");
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
 
 
-
-
-
-
-
-
-
-
-
-
-
-module.exports = connection;
+// Export the database functions for the controller (burger_controller.js)
+module.exports = burger;
