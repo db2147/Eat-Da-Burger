e1722aebc4e4911be93d430c737497ef3343fdd0
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-22 16:16:47 -0600

    adding more

--- server.js
+++ server.js
@@ -4,36 +4,48 @@ var bodyParser = require("body-parser");
 
 // create our server
 var app = express();
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({
     extended: false
-}))
+}));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Set Method Override
 app.use(methodOverride('_method'));
 app.engine('handlebars',exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
-// create connection to port
-var port = 3000;
+// Import routes and give the server access to them.
+var routes = require(./controllers/burgersController.js)
+
+app.use("/", routes);
 app.listen(port);
 
  
 
-app.get('/',function(req,res){
-    connection.query('SELECT * FROM burgers;',function(err,data){
-        res.render('index',{movies:data});
-    })
-})
+// app.get('/',function(req,res){
+//     connection.query('SELECT * FROM burgers;',function(err,data){
+//         res.render('index',{movies:data});
+//     })
+// })
 
-app.post('/create', function(req,res){
-    connection.query('INSERT INTO burgers (burger) VALUES (?);s', [req.body.burger], function(err,result){
-        if(err)throw err;
-        res.redirect('/');   // redirects to the main page when the query goes off
-    })
-})
+// app.post('/create', function(req,res){
+//     connection.query('INSERT INTO burgers (burger) VALUES (?);s', [req.body.burger], function(err,result){
+//         if(err)throw err;
+//         res.redirect('/');   // redirects to the main page when the query goes off
+//     })
+// })
 
-app.put('/update',function(req,res){
-    connection.query('UPDATE burgers SET burger = ? WHERE id = ?;', [req.body.
-    burger, req.body.id], function(err,results){
-        if(err)throw err;
-        res.redirect('/');
-    })
-})
+// app.put('/update',function(req,res){
+//     connection.query('UPDATE burgers SET burger = ? WHERE id = ?;', [req.body.
+//     burger, req.body.id], function(err,results){
+//         if(err)throw err;
+//         res.redirect('/');
+//     })
+// })
