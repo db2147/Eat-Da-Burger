91b14c480229d0ca91b0b14c1edbe60a09598220
David Bierma <31113045+db2147@users.noreply.github.com>
2018-01-04 12:02:11 -0600

    heroku update

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,23 @@
+import { createConnection } from "../../../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/mysql";
+
 /* 
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require("mysql");
 
 // create mysql connection
-var connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'burger_db'
-});
-
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+var connection; 
   
-  // export the node/mysql connection
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host:'localhost',
+      user:'root',
+      password:'root',
+      database:'burger_db'
+  });
+};
+
+  connection.connect();
   module.exports = connection;
